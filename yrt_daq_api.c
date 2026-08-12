c780e9d1ba4a277e5677b4a67e3c3c42e3f89297
TheLastt5 <mre_ylmz@outlook.com>
2026-08-12 15:06:45 +0300

    Update yrt_daq_api.c

--- yrt_daq_api.c
+++ yrt_daq_api.c
@@ -18,8 +18,8 @@ static I2C_HandleTypeDef *daq_i2c;
 YRT_Status_t YRT_DAQ_API_Init_Sensors(I2C_HandleTypeDef *hi2c_ptr) {
     if (hi2c_ptr == NULL) return YRT_ERROR;
     daq_i2c = hi2c_ptr;
-    
-    /* İçeride HAL fonksiyonlarını kullanıyoruz ama dışarıya YRT_OK / YRT_ERROR dönüyoruz */
+
+    /* HAL/Driver seviyesindeki fonksiyon kendi tipini döner, biz YRT tipine çevirip üst katmana iletiriz */
     if (ADS1115_Init(daq_i2c, ADS1115_DATA_RATE_128, ADS1115_PGA_TWO) != HAL_OK) {
         return YRT_ERROR;
     }
