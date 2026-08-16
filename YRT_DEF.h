86fd9db507878df905c6ee4e1bdbcaa437841aac
Ali Okan Yeşilyurt <okan.yesilyurt@std.yildiz.edu.tr>
2026-08-16 17:01:41 +0300

    Tercihe bağlı global değişken ve definelar bulunduran dosya

--- YRT_DEF.h
+++ YRT_DEF.h
@@ -1,12 +1,12 @@
 /**
-* @file YRT_JR_DEF.h
+* @file YRT_DEF.h
 * @author Ali Okan Yeşilyurt
-* @brief Header file for YRT JR definitions
+* @brief Header file for YRT definitions
 * @version BETA
 * @copyright Copyright (c) 2026
 */
-#ifndef YRT_JR_DEF_H
-#define YRT_JR_DEF_H
+#ifndef YRT_DEF_H
+#define YRT_DEF_H
 
 #include <stdint.h>
 
@@ -18,43 +18,50 @@
 #define PRC_CAN_ID 0X103
 // TODO: Kartların adını öğrenince ID'leri güncelle.
 
-#define FLOAT_TO_UINT16(X) (uint16_t)(X * 1000)
-#define UINT16_TO_FLOAT(X) (float)(X / 1000.0f)
+#define FLOAT_TO_UINT16(X) ((uint16_t)((X) * 1000.0f))
+#define UINT16_TO_FLOAT(X) ((float)((X) / 1000.0f))
 
 /**
  * @brief RESULT CODES
  */
 typedef enum {
-    YRT_JR_OK = 0,
-    YRT_JR_ERROR = 1,
-    YRT_JR_TIMEOUT = 2,
-    YRT_JR_INVALID_PARAM = 3,
-    YRT_JR_INIT_ERROR = 4,
-    YRT_JR_COMM_ERROR = 5,
-    YRT_JR_BARO_ERROR = 6,
-    YRT_JR_BARO_HARD_ERROR = 7,
-    YRT_JR_IMU_ERROR = 8,
-    YRT_JR_IMU_HARD_ERROR = 9,
-    YRT_JR_CAN_ERROR = 10,
+    YRT_OK = 0,
+    YRT_ERROR = 1,
+    YRT_TIMEOUT = 2,
+    YRT_INVALID_PARAM = 3,
+    YRT_INIT_ERROR = 4,
+    YRT_COMM_ERROR = 5,
+    YRT_BARO_ERROR = 6,
+    YRT_BARO_HARD_ERROR = 7,
+    YRT_IMU_ERROR = 8,
+    YRT_IMU_HARD_ERROR = 9,
+    YRT_CAN_ERROR = 10,
     YRT_JR_SD_ERROR = 11,
-} YRT_JR_Result_t;
+} YRT_Status_t;
 
 /**
  * @brief IMU datas
  */
 typedef struct {
-    int16_t accel_x;        
-    int16_t accel_y;        
-    int16_t accel_z;      
-    float   accel_z_avg;    
-    float   accel_total;    
-} YRT_IMU_Data_t;
+    float   accel_x;        /* m/s^2 */
+    float   accel_y;        /* m/s^2 */
+    float   accel_z;        /* m/s^2 */
+    float   accel_z_avg;    /* m/s^2, filtreli */
+    float   accel_total;    /* m/s^2, bileske */
+} YRT_Flight_IMU_t;
+
+typedef struct {
+    float x; 
+    float y; 
+    float z; 
+} YRT_Flight_Gyro_t;
 
 /**
  * @brief Barometer datas
  */
 typedef struct {
     float pressure_current;     
+    float pressure_start;
     float pressure_filtered;   
     float pressure_initial;     
     
@@ -64,15 +71,16 @@ typedef struct {
     float altitude_current;     
     float altitude_filtered;    
     float altitude_initial;    
-    float altitude_relative;    
+    float altitude_relative; 
+    float altitude_start;   
     float altitude_max;         
-} YRT_Baro_Data_t;
+} YRT_Flight_Baro_t;
 
 typedef struct {
     float latitude;
     float longitude;
     float altitude;
-} YRT_GPS_Data_t;
+} YRT_Flight_GPS_t;
 
 /**
  * @brief Flight state datas
@@ -80,9 +88,9 @@ typedef struct {
 typedef struct {
     float    vertical_velocity; 
     uint32_t previous_time;    
-    uint32_t apogee_time;       
+    uint32_t landing_time;       
     uint32_t fall_counter;      
-} YRT_FlightState_t;
+} YRT_FlightSpec_t;
 
 /**
  * @brief Telemetry  packet with pragma 
@@ -90,14 +98,14 @@ typedef struct {
 #pragma pack(push, 1)
 typedef struct {
     uint32_t          timestamp_ms; 
-    YRT_IMU_Data_t    imu;          
-    YRT_Baro_Data_t   baro;        
-    YRT_FlightState_t state;
-    YRT_GPS_Data_t     gps;        
+    YRT_Flight_IMU_t    imu;          
+    YRT_Flight_Baro_t   baro;        
+    YRT_FlightSpec_t state;
+    YRT_Flight_GPS_t     gps;        
+    YRT_Flight_Gyro_t euler;
     uint16_t          crc;          
-} YRT_TelePacket_t;
+} YRT_FlightData_t;
 #pragma pack(pop)
-
 
 /**
  * @brief Conventer unions
@@ -114,4 +122,4 @@ typedef union
 } double_to_u8;
 //TODO: Bu dönüştürücülerin sayısı ihtiyaca göre artacak.
 
-#endif /* YRT_JR_DEF_H */
+#endif /* YRT_DEF_H */
